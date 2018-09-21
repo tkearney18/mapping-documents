7fbe623e58c86761c072fa753f223448db0d6064
Troy Kearney <tkearney18@gmail.com>
2018-09-21 01:13:47 -0400

    Update to properly map fields to values without nexted for loops.

--- mapping-document.py
+++ mapping-document.py
@@ -6,7 +6,7 @@ import csv
 path = 'sample docs/'            
 all_files = os.listdir(path) 
 
-print(all_files)
+#print(all_files)
 
 wbName = 'Plan Doc Map.xlsx'
 sheetName = 'Sheet1'
@@ -22,19 +22,32 @@ for colidx, cell in enumerate(row):
     elif cell.value == "File Name":
         fileNameList = sh.col_values(colidx, 1)
         
-docMap = {}
+docMap = []
+i = 0
 for materialId in materialIdList:
-    for fileName in fileNameList:
-        index = str(materialId).rstrip('0').rstrip('.')
-        docMap[index] = [fileName]
-
-for listItem in docMap:
-    for doc in all_files:
-        if listItem in doc:
-            docMap[str(materialId).rstrip('0').rstrip('.')].append(doc)  
+    index = str(materialId).rstrip('0').rstrip('.')
+    docItem = {'Material ID': index, 'Screen Name': fileNameList[i], 'Contract ID': contractIdList[i]}
+    i += 1
+    docMap.append(docItem)
 print(docMap)
+for listItem in docMap:
+    indices = [i for i, f in enumerate(all_files) if listItem.get('Material ID') in f]
+    if indices:
+        listItem['File Name'] = all_files[indices[0]]
+    else:
+        listItem['File Name'] = ''
+    #if listItem.get('Material ID') in all_files:
+        #print(listItem.get('Material ID'))
+    # for doc in all_files:
+    #     print(listItem.get('Material ID'))
+    #     if listItem.get('Material ID') in doc:
+    #         listItem['File Name'] = doc
+    #     else:
+    #         listItem['File Name'] = ''
+#print(docMap)
+keys = docMap[0].keys()
 with open('anyting.csv', 'w') as outputFile:
     #dictWriter = csv.writer(outputFile)
-    dictWriter = csv.DictWriter(outputFile,keys)
+    dictWriter = csv.DictWriter(outputFile,keys,lineterminator='\n')
     dictWriter.writeheader()
-    dictWriter.writerows(docList)
+    dictWriter.writerows(docMap)
